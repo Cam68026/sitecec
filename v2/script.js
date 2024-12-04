6c8257abd5a7fd4aa853ee9a4695d952ca787965
Darklord <camilleelsaesser68@gmail.com>
2024-12-04 23:23:29 +0100

    Création de la galearie avec un lightbox

--- v2/script.js
+++ v2/script.js
@@ -195,3 +195,16 @@ function supprimerArticle(fileName, articleElement) {
         }
     });
 }
+function openLightbox(imageSrc) {
+    const lightbox = document.getElementById('lightbox');
+    const lightboxImg = document.getElementById('lightbox-img');
+  
+    lightboxImg.src = imageSrc;
+    lightbox.style.display = 'flex';
+  }
+  
+  function closeLightbox() {
+    const lightbox = document.getElementById('lightbox');
+    lightbox.style.display = 'none';
+  }
+  
